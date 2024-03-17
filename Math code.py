f8f7ff718b44838cde3bad0a6e965e9ded697207
Littlegeorge6 <163775791+Littlegeorge6@users.noreply.github.com>
2024-03-17 20:49:34 +0000

    Add files via upload

---
+++ Math code.py
@@ -0,0 +1,10 @@
+print ("What is side A?")
+SA = int(input())
+print("what is side B?")
+SB= int(input())
+print ("what is side C?")
+SC = int(input())
+
+area = SA * SB * SC
+
+print("Your area is",area)
